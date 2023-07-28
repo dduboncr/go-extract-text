e7ac1b4d3123cda535e496ba94343520bae17c28
Diego Dubon <diego.dubon@meetkai.com>
2023-07-28 14:52:36 -0600

    feature(update): wip

---
+++ server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"extract/controllers"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+)
+
+
+func SetupRoutes(app *fiber.App) {
+	app.Post("/process", controllers.Process)
+
+}
+
+
+func main() {
+
+	app := fiber.New()
+
+	SetupRoutes(app);
+
+	app.Use(recover.New())
+	app.Use(cors.New())
+	app.Use(logger.New(logger.Config{
+		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
+	}))
+	
+
+	app.Listen(":3000")
+}
